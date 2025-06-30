19feb19aead519535412d31daac8fc7d1b5cfe2f
alfan06 <alfannoufal@gmail.com>
2025-06-30 12:04:15 +0700

    v2.9.0

---
+++ src/Import/Listeners/SendExportCompletedNovaNotification.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AdevPmftc\NovaDataSync\Import\Listeners;
+
+use AdevPmftc\NovaDataSync\Import\Events\ExportCompletedEvent;
+use Laravel\Nova\Notifications\NovaNotification;
+
+class SendExportCompletedNovaNotification
+{
+    public function handle(ExportCompletedEvent $event): void
+    {
+        if (!$event->export->user) {
+            return;
+        }
+
+        $event->import->user->notify(
+            NovaNotification::make()
+                ->message('Your export has completed.')
+                ->icon('check-circle')
+                ->type('success')
+        );
+    }
+}
